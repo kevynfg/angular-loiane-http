bf05c786020ccea5e2f46aa1b54bc4c7fb82131f
kevynfg <kevynredlotus@gmail.com>
2021-02-24 18:10:13 -0300

    - rxjs operators (important)

--- src/app/reactive-search/lib-search/lib-search.component.ts
+++ src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,8 +1,16 @@
-import { map, tap } from 'rxjs/operators';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  switchMap,
+  tap,
+} from 'rxjs/operators';
 import { HttpClient, HttpEvent, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-lib-search',
@@ -14,14 +22,36 @@ export class LibSearchComponent implements OnInit {
   readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
   results$: Observable<any>;
   total: number;
+  FIELDS = 'name,description,version,homepage';
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // Filtro para pesquisa
+    this.results$ = this.queryField.valueChanges.pipe(
+      map((value) => value.trim()),
+      filter((value) => value.length > 1),
+      debounceTime(500),
+      distinctUntilChanged(),
+      // tap((value) => console.log(value))
+      switchMap((value) =>
+        this.http.get(this.SEARCH_URL, {
+          params: {
+            // value -> valor filtrado da query acima
+            search: value,
+            fields: this.FIELDS,
+          },
+        })
+      ),
+      tap((response: any) => (this.total = response.total)),
+      map((res: any) => res.results)
+    );
+  }
 
   onSearch() {
     const fields = 'name,description,version,homepage';
     let value = this.queryField.value;
+    // tslint:disable-next-line: no-conditional-assignment
     if (value && (value = value.trim()) !== '') {
       value = value.trim();
 

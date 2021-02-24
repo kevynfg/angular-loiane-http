09a1a9aa32d125881a8f3b2237381f838dcf9e27
kevynfg <kevynredlotus@gmail.com>
2021-02-24 15:29:54 -0300

    rxjs-operator, uploadProgress, filterResponse

---
+++ src/app/shared/rxjs-operators.ts
@@ -0,0 +1,20 @@
+import { HttpEvent, HttpEventType, HttpResponse } from '@angular/common/http';
+import { Event } from '@angular/router';
+import { pipe } from 'rxjs';
+import { filter, map, pluck, tap } from 'rxjs/operators';
+
+// tslint:disable-next-line: typedef
+export function filterResponse<T>() {
+  return pipe(
+    filter((event: any) => event.type === HttpEventType.Response),
+    map((res: HttpResponse<T>) => res.body)
+  );
+}
+
+export function uploadProgress<T>(callback: (progress: number) => void) {
+  return tap((event: HttpEvent<T>) => {
+    if (event.type === HttpEventType.UploadProgress) {
+      callback(Math.round((event.loaded * 100) / event.total));
+    }
+  });
+}
--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,7 +1,9 @@
+import { filter } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Component, Input, OnInit } from '@angular/core';
 import { UploadService } from '../upload.service';
 import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { filterResponse, uploadProgress } from 'src/app/shared/rxjs-operators';
 
 @Component({
   selector: 'app-upload-file',
@@ -39,17 +41,24 @@ export class UploadFileComponent implements OnInit {
     if (this.inputFile && this.inputFile.size > 0) {
       this.fileService
         .upload(this.inputFile, environment.BASE_URL + '/upload')
-        .subscribe((event: HttpEvent<object>) => {
-          // HttpEventType;
-          console.log(event);
-          if (event.type === HttpEventType.Response) {
-            console.log('Upload Concluído.');
-          } else if (event.type === HttpEventType.UploadProgress) {
-            const percentDone = Math.round((event.loaded * 100) / event.total);
-            console.log('Progresso: ', percentDone);
-            this.progress = percentDone;
-          }
-        });
+        .pipe(
+          uploadProgress((progress) => {
+            console.log(progress);
+            this.progress = progress;
+          }),
+          filterResponse()
+        )
+        .subscribe((response) => console.log('Upload concluído.'));
+
+      // .subscribe((event: HttpEvent<object>) => {
+      //   console.log(event);
+      //   if (event.type === HttpEventType.Response) {
+      //     console.log('Upload Concluído.');
+      //   } else if (event.type === HttpEventType.UploadProgress) {
+      //     const percentDone = Math.round((event.loaded * 100) / event.total);
+      //     this.progress = percentDone;
+      //   }
+      // });
     }
   }
 
